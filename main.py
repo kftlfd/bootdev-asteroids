d13a45174d7dfeab87076af993f8d1ac3d735585
kftlfd <kftlfd@ex.ua>
2026-01-12 21:00:04 +0200

    add game clock

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     screen = pygame.display.set_mode(
         (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -23,6 +26,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
